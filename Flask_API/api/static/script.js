1d7fd27b0be92a22626396ffed7146c0814e665e
jessemeekins <jessemeekins@me.com>
2023-03-02 14:27:21 -0600

    Updated Add and Delete Buttons

--- Flask_API/api/static/script.js
+++ Flask_API/api/static/script.js
@@ -6,22 +6,46 @@ const homeButton = document.getElementById('home');
 
 const addAlsButton = document.getElementById('add-als-count');
 const rmAlsButton = document.getElementById('rm-als-count');
-let alsCount = document.getElementById('als-count').innerHTML;
+let alsCount = document.getElementById('als-count').innerText;
+let newCount = document.getElementById('als-count');
 
-
-const alsCounter = addAlsButton.onclick = () => {
-    alsCount++;
-    
-    alsCount.innerHTML = alsCount;
-    console.log(alsCount)
+const alsAdd = addAlsButton.onclick = () => {
+    alsCount ++;
+    newCount.innerHTML = alsCount
+}
+const alsRm = rmAlsButton.onclick = () => {
+    alsCount --;
+    newCount.innerHTML = alsCount
 }
 
-const buttonCount = document.getElementById('count');
+
+const addBlsButton = document.getElementById('add-bls-count');
+const rmBlsButton = document.getElementById('rm-bls-count');
+let blsCount = document.getElementById('bls-count').innerText;
+let newBlsCount = document.getElementById('bls-count');
+
+const blsAdd = addBlsButton.onclick = () => {
+    blsCount ++;
+    newBlsCount.innerHTML = blsCount;
+}
+const blsRm = rmBlsButton.onclick = () => {
+    blsCount --;
+    newBlsCount.innerHTML = blsCount;
+}
+
+
+const addBtnCount = document.getElementById('add-counter-count');
+const rmBtnCount = document.getElementById('rm-counter-count');
 const getCount = document.getElementById('counter');
 let count = 0;
 
-const counter = buttonCount.onclick = () => {
-    count+=1;
+const addCounter = addBtnCount.onclick = () => {
+    count++;
+    getCount.innerHTML = count;
+    console.log(count)
+}
+const rmCounter = rmBtnCount.onclick = () => {
+    count--;
     getCount.innerHTML = count;
     console.log(count)
 }
